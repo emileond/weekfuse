8cf3d7c989f31bef74f26a922de0458b08f74950
Emilio Sanchez <sonarart@gmail.com>
2025-07-01 21:55:49 -0600

    notes empty state

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -6,6 +6,7 @@ import NoteCard from '../components/notes/NoteCard.jsx';
 import NewNoteCard from '../components/notes/NewNoteCard.jsx';
 import { useState } from 'react';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
+import EmptyState from '../components/EmptyState';
 
 const Notes = () => {
     const [currentWorkspace] = useCurrentWorkspace();
@@ -33,21 +34,30 @@ const Notes = () => {
                     {isOpen && (
                         <NewNoteCard onCancel={handleOnCancel} onSuccess={handleOnNewNote} />
                     )}
-                    {notes
-                        ?.sort((a, b) => {
-                            // First sort by pinned status (pinned notes first)
-                            if (a.is_pinned && !b.is_pinned) return -1;
-                            if (!a.is_pinned && b.is_pinned) return 1;
-                            // Then sort by created_at (newest first)
-                            return new Date(b.created_at) - new Date(a.created_at);
-                        })
-                        .map((note) => (
-                            <NoteCard
-                                key={note.id}
-                                note={note}
-                                currentWorkspace={currentWorkspace}
-                            />
-                        ))}
+                    {notes?.length ? (
+                        notes
+                            .sort((a, b) => {
+                                // First sort by pinned status (pinned notes first)
+                                if (a.is_pinned && !b.is_pinned) return -1;
+                                if (!a.is_pinned && b.is_pinned) return 1;
+                                // Then sort by created_at (newest first)
+                                return new Date(b.created_at) - new Date(a.created_at);
+                            })
+                            .map((note) => (
+                                <NoteCard
+                                    key={note.id}
+                                    note={note}
+                                    currentWorkspace={currentWorkspace}
+                                />
+                            ))
+                    ) : (
+                        <EmptyState
+                            title="No notes found"
+                            description="Create a new note to get started"
+                            primaryAction="New note"
+                            onClick={() => setIsOpen(true)}
+                        />
+                    )}
                 </div>
             </PageLayout>
         </AppLayout>
